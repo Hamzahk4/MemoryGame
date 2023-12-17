5fafbe797270e408e3c7c5cdd1a271af524b1c4e
HamzahK <hamzahk4@nycstudents.net>
2023-12-17 12:42:20 -0500

    Proj

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.lang.Thread;
+public class Main {
+    public static void main(String[] args){
+        Intro intro = new Intro();
+        intro.Welcome();
+
+    }
+}

bdf7486c3bdeae9853a73309adbf1f2e918bd67b
BT_1N3_33 <samuell180@nycstudents.net>
2024-01-09 15:37:13 -0500

    g

--- ImageSwitcher.java
+++ ImageSwitcher.java
@@ -2,16 +2,17 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ImageSwitcher extends Frame {
-    private Image image0, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18;
-    private final Image[] flics;
-    private Image[] inUseFlics = new Image[12];
+    private Image image0, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13;
+    private Image[] flics = new Image[13];
+    private Image[] inUseFlics = new Image[24];
     private final ArrayList<Image> possibleUseFlics = new ArrayList<Image>();
-    private ArrayList<Image> tempFlics;
-    private Image[] boardPositions;
-    private int[] xCoords = {370,635,900,1665,1430,1655};
-    private int[] yCoords = {7, 275,543,811, };
+    private int[] xCoords = {370, 635, 900, 1665, 1430, 1655};
+    private int[] yCoords = {7, 275, 543, 811};
     private int imageCount = 1;
+
     public ImageSwitcher() {
         image0 = Toolkit.getDefaultToolkit().getImage(".idea/Image0.jpg");
         image1 = Toolkit.getDefaultToolkit().getImage(".idea/Image1.jpg");
@@ -28,24 +29,50 @@ public class ImageSwitcher extends Frame {
         image12 = Toolkit.getDefaultToolkit().getImage(".idea/Image12.jpg");
         image13 = Toolkit.getDefaultToolkit().getImage(".idea/Image13.jpg");
 // settimg up arrays
-        flics = new Image[] {image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13};
-        for (int i = 0; i < flics.length; i++){possibleUseFlics.add(flics[i]);}
-        tempFlics = possibleUseFlics;
-//set up in use array and photos that will be used for round\
-        for (int i = 0; i < 12; i++) {
-            int num = (int) (Math.random() * tempFlics.size());
-            inUseFlics[i] = tempFlics.get(num);
-            tempFlics.remove(num);
+        flics = new Image[]{image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13};
+        for (int i = 0; i < flics.length; i++) {
+            possibleUseFlics.add(flics[i]);
         }
-        //board array
+        //flics and possibleUseFlics hold same data. Flic is array, puf is arrayList
 
-        // to come soon
+//set up in use array and photos that will be used for round\
+        for (int i = 0; i < 23; i += 2) {
+            int num = (int) (Math.random() * possibleUseFlics.size());
+            inUseFlics[i] = possibleUseFlics.get(num);
+            inUseFlics[i + 1] = possibleUseFlics.get(num);
+            possibleUseFlics.remove(num);
+        }
+        // shuffle is used to shuffle our array to board positions
+        shuffle(inUseFlics);
+        construct();
+    }//end of constructor
+
+    private void printBoard(Image[] ar,Graphics g) {
+        for (int i = 0; i < yCoords.length; i++) {
+            for (int j = 0; j < xCoords.length; j++) {
+                g.drawImage(flics[imageCount - 1], xCoords[j], yCoords[i], 265, 265, this);
+            }
+        }
+    }
+
+    public void shuffle(Image[] ar) {
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = ar.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            Image a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+    }
+
+    // to come soon
+    public void construct() {
         setTitle("Memory Game");
         setSize(1920, 1080);
         addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 imageCount++;
-                repaint();
+                printBoard(inUseFlics);
             }
         });
         addWindowListener(new WindowAdapter() {
@@ -54,10 +81,11 @@ public class ImageSwitcher extends Frame {
             }
         });
     }
+
     public void paint(Graphics g) {
 
-        g.drawImage(flics[imageCount-1], 0, 0, 260, 260, this);
-        }
+        g.drawImage(flics[imageCount - 1], 0, 0, 260, 260, this);
+    }
 
     }
 //https://code-with-me.global.jetbrains.com/F4UfI5Rq-ZZTC3ZV9aVxwA#p=IC&fp=9ACA5DB17627033B459E5C57C493396F6083AF2ACB0E758E5CD6DE6DD8854129

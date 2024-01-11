1b4ca3690c2f06b36ec89c24c4bbaa9d623b305c
Hamzahk4 <110351211+Hamzahk4@users.noreply.github.com>
2024-01-10 22:36:06 -0500

    Update ImageSwitcher.java

--- ImageSwitcher.java
+++ ImageSwitcher.java
@@ -4,18 +4,34 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class ImageSwitcher extends Frame{
-//    Frame frame;
-    Graphics g;
+public class ImageSwitcher extends Frame {
     private Image image0, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13;
     private Image[] flics = new Image[13];
     private Image[] inUseFlics = new Image[24];
-    private final ArrayList<Image> possibleUseFlics = new ArrayList<Image>();
-    private int[] xCoords = {370, 635, 900, 1665, 1430, 1655};
-    private int[] yCoords = {7, 275, 543, 811};
+    private final ArrayList<Image> possibleUseFlics = new ArrayList<>();
+    private int[] xCoords = {370, 635, 900, 1165, 1430, 1695}; // Adjusted x coordinates for 4 columns
+    private int[] yCoords = {7, 282, 557, 832};               // Adjusted y coordinates for 4 rows
     private int imageCount = 1;
 
     public ImageSwitcher() {
+        loadImages();
+        initializeArrays();
+
+        setVisible(true);
+        setSize(1920, 1080);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        shuffle(inUseFlics);
+        repaint(); // Trigger the paint method after loading the images
+    }
+
+    private void loadImages() {
         image0 = Toolkit.getDefaultToolkit().getImage(".idea/Image0.jpg");
         image1 = Toolkit.getDefaultToolkit().getImage(".idea/Image1.jpg");
         image2 = Toolkit.getDefaultToolkit().getImage(".idea/Image2.jpg");
@@ -30,36 +46,28 @@ public class ImageSwitcher extends Frame{
         image11 = Toolkit.getDefaultToolkit().getImage(".idea/Image11.jpg");
         image12 = Toolkit.getDefaultToolkit().getImage(".idea/Image12.jpg");
         image13 = Toolkit.getDefaultToolkit().getImage(".idea/Image13.jpg");
-// settimg up arrays
+    }
+
+    private void initializeArrays() {
         flics = new Image[]{image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13};
         for (int i = 0; i < flics.length; i++) {
             possibleUseFlics.add(flics[i]);
         }
-        //flics and possibleUseFlics hold same data. Flic is array, puf is arrayList
 
-//set up in use array and photos that will be used for round\
         for (int i = 0; i < 23; i += 2) {
             int num = (int) (Math.random() * possibleUseFlics.size());
             inUseFlics[i] = possibleUseFlics.get(num);
             inUseFlics[i + 1] = possibleUseFlics.get(num);
             possibleUseFlics.remove(num);
         }
-        setVisible(true);
+    }
 
-        setSize(1920,1080);
-        addWindowListener(new WindowAdapter() {
-                              @Override
-                              public void windowClosing(WindowEvent e) {
-                                  System.exit(0);
-                              }
-                          });
-            // shuffle is used to shuffle our array to board positions
-        shuffle(inUseFlics);
-        g = getGraphics();
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
         g.drawImage(image0, 100, 100, 265, 265, null);
-        System.out.println(g);
         printBoard(inUseFlics, g);
-    }//end of constructor
+    }
 
     private void printBoard(Image[] ar, Graphics g) {
         int count = 0;
@@ -70,7 +78,7 @@ public class ImageSwitcher extends Frame{
             }
         }
     }
-    // randomizes where each image is within the 24
+
     public void shuffle(Image[] ar) {
         Random rnd = ThreadLocalRandom.current();
         for (int i = ar.length - 1; i > 0; i--) {
@@ -81,21 +89,9 @@ public class ImageSwitcher extends Frame{
         }
     }
 
-    // to come soon
-    public void construct() {
-//        setTitle("Memory Game");setSize(100, 1080);
-
-        addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                imageCount++;
-
-            }
-        });
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            ImageSwitcher imageSwitcher = new ImageSwitcher();
         });
     }
 }
-//https://code-with-me.global.jetbrains.com/qT23fhMSmbMBtEmX0KCcOw#p=IC&fp=519A6C9894CC7F47097267E648EE6AFDBC4248D9C3AFE83137AE785C8E7E4417

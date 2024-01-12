2d54b8e81d44e5eac8926a0a9b053174aabb2b0a
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2024-01-12 15:33:52 -0500

    Update ImageSwitcher.java

--- ImageSwitcher.java
+++ ImageSwitcher.java
@@ -1,5 +1,6 @@
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.*;
@@ -10,35 +11,34 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ImageSwitcher extends JFrame {
+public class ImageSwitcher extends JFrame implements ActionListener {
     private Image image0, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13;
     private Image[] flics = new Image[13];
-    private Image[] inUseFlics = new Image[24];
+    public static Image[] inUseFlics = new Image[24];
     private final List<Image> possibleUseFlics = new ArrayList<>();
-    private final int[] xCoords = {315,585 ,855, 1125, 1395, 1665}; // x coordinates for 6 columns
-    private final int[] yCoords = {0, 265, 530, 795};               // y coordinates for 4 rows
+    private static final int[] xCoords = {315,585 ,855, 1125, 1395, 1665}; // x coordinates for 6 columns
+    private static final int[] yCoords = {0, 265, 530, 795};               // y coordinates for 4 rows
     private int imageCount = 1;
+    //private ButtonTest button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12,button13, button14, button15, button16, button17, button18, button19, button20, button21, button22, button23, button24;
+    private ButtonTest[] butts = new ButtonTest[24];
 
+    public Image[] getFlics()
+    {
+        return inUseFlics;
+    }
     public ImageSwitcher() {
         loadImages();
         initializeArrays();
 //        setResizable(false);
-        pack();
-
-
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         shuffle(inUseFlics);
-
+        pack();
         revalidate(); // Trigger the paint method after loading the images
-
-        Stopwatch stopwatch = new Stopwatch();
-
+       // Stopwatch stopwatch = new Stopwatch();
         // Create and add buttons
-       Buttons buttons = new Buttons();
+        makeButtons();
         setVisible(true);
-        setExtendedState(getExtendedState()|JFrame.MAXIMIZED_BOTH);
+//        setExtendedState(getExtendedState()|JFrame.MAXIMIZED_BOTH);
 
 //       buttons.addToFrame(this);
     }
@@ -82,16 +82,16 @@ public class ImageSwitcher extends JFrame {
         }
     }
 
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            new ImageSwitcher();
-        });
-    }
+//    public static void main(String[] args) {
+//        EventQueue.invokeLater(() -> {
+//            new ImageSwitcher();
+//        });
+//    }
 
-    public int[] getxCoords(){
+    public static int[] getXCoords(){
         return xCoords;
     }
-    public int[] getyCoords(){
+    public static int[] getYCoords(){
         return yCoords;
     }
 
@@ -111,13 +111,18 @@ public class ImageSwitcher extends JFrame {
         image12 = Toolkit.getDefaultToolkit().getImage(".idea/Image12.jpg");
         image13 = Toolkit.getDefaultToolkit().getImage(".idea/Image13.jpg");
     }
-
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e){};
 
-        if (e.getSource() == button1) {
-            g.drawImage(image0, xCoords[j], yCoords[i], 255, 255, null);
-        }
+   public void makeButtons(){
+       int count = 0;
+       for (int i = 0; i < yCoords.length; i++) {
+           for (int j = 0; j < xCoords.length; j++) {
+               butts[count] = new ButtonTest(j, i);
+               count++;
+           }
+       }
+}
 }
 
-https://code-with-me.global.jetbrains.com/14ujDaszN0jje35kvqj3Eg#p=IC&fp=2825E2804BA4315AB540CE105FFDF40E0A54C852752C076847716F53FD1BA632
+//https://code-with-me.global.jetbrains.com/GVsdMpBmVI_b26bLEVgIuQ#p=IC&fp=2825E2804BA4315AB540CE105FFDF40E0A54C852752C076847716F53FD1BA632

45a707406e0237b84498023166736fd8306321d6
Hamzahk4 <110351211+Hamzahk4@users.noreply.github.com>
2024-01-19 15:20:46 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,6 @@ import java.awt.event.*;
 public class Main {
     public static void main(String[] args)
     {
-//        EventQueue.invokeLater(() -> {
         ImageSwitcher imageSwitcher = new ImageSwitcher();
-//            imageSwitcher.makeButtons(); // Add buttons to the frame
-//        });
-
-
-
-
-
     }
 }

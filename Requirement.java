bb26d473ccc41301e22c217a32678acbd1c41ae8
samlewin19 <144487001+samlewin19@users.noreply.github.com>
2024-01-22 15:31:54 -0500

    Update Requirement.java

--- Requirement.java
+++ Requirement.java
@@ -2,5 +2,5 @@ public class Requirement {
     private Person pp;
     private int dinasours = 6;
     private boolean epstienKilledHimself = false;
-
+//probably can take this class out because stopwatch has an object of another class
 }
